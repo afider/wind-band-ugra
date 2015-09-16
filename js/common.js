c2ddb8b8d1f39a2c1d4cd0a22828ef1b6659cc20
Evgeniy Beskrovnyy <e.beskrovnyy@gmail.com>
2015-09-16 16:40:14 +0300

    main navigation chagnes

--- js/common.js
+++ js/common.js
@@ -104,14 +104,23 @@
 
 
         // анимация родительских пунктов меню
-        $(".main-nav__i_parent").on('click', function(e) {
+        var parentItem = $(".main-nav__i_parent");
+        var parentItemCtrl = $(".main-nav__i_parent .main-nav__a");
+
+        parentItemCtrl.on('click', function(e) {
             e.preventDefault();
 
             var self = $(this),
                 target = $('.' + self.data('nav'));
 
-            self.toggleClass('main-nav__i_open');
+            self.parent().toggleClass('main-nav__i_open');
 
+        });
+
+        $(document).click(function(e){
+            if ($(e.target).parents().andSelf().filter('.main-nav').length != 1) {
+                parentItem.removeClass('main-nav__i_open');
+            }
         });
 
         // анимация мобильного меню
